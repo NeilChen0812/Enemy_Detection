61fb5182e0762a03408e41560da4e963b5b52b8a
Chen, Tai-Lin <neil.chentl@gmail.com>
2024-01-18 00:23:10 +0800

    update train faction

--- train_faction_2.py
+++ train_faction_2.py
@@ -1,22 +1,9 @@
-# -*- coding: utf-8 -*-
- 
 import os
-import sys
-import random
-import math
-import re
-import time
 import numpy as np
 import cv2
-# import matplotlib
-# import matplotlib.pyplot as plt
-import tensorflow as tf
 from mrcnn.config import Config
-# import utils
 from mrcnn import model as modellib, utils
-from mrcnn import visualize
 import yaml
-from mrcnn.model import log
 from PIL import Image
  
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -29,62 +16,34 @@ MODEL_DIR = os.path.join(ROOT_DIR, "logs")
  
 iter_num = 0
 
-# Customize
 # Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "weights/mask_rcnn_people_v4.h5")
+PEOPLE_MODEL_PATH = os.path.join(ROOT_DIR, "weights/mask_rcnn_people_v4.h5")
+if not os.path.exists(PEOPLE_MODEL_PATH):
+    print("Model not found in path:", PEOPLE_MODEL_PATH)
 
-# Download COCO trained weights from Releases if needed
-# if not os.path.exists(COCO_MODEL_PATH):
-#     utils.download_trained_weights(COCO_MODEL_PATH)
-
-# Customize
-class ShapesConfig(Config):
-    """Configuration for training on the toy shapes dataset.
-    Derives from the base Config class and overrides values specific
-    to the toy shapes dataset.
-    """
+class FactionsConfig(Config):
     # Give the configuration a recognizable name
-    NAME = "shapes"
-    
-    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
-    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    NAME = "factions"
     GPU_COUNT = 1
-    IMAGES_PER_GPU = 3
+    IMAGES_PER_GPU = 2
     
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 2  # background + class number
- 
-    # Use small images for faster training. Set the limits of the small side
-    # the large side, and that determines the image shape.
+    NUM_CLASSES = 1 + 2  # background + 2 factions
     IMAGE_MIN_DIM = 256
     IMAGE_MAX_DIM = 1024
-
-    # Use smaller anchors because our image and objects are small
-    # RPN_ANCHOR_SCALES = (8 * 6, 16 * 6, 32 * 6, 64 * 6, 128 * 6)  # anchor side in pixels
-    RPN_ANCHOR_SCALES = (16 * 6, 32 * 6, 64 * 6, 128 * 6, 256 * 6)
- 
-    # Reduce training ROIs per image because the images are small and have
-    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    RPN_ANCHOR_SCALES = (8 * 6, 16 * 6, 32 * 6, 64 * 6, 128 * 6)  # anchor side in pixels
     TRAIN_ROIS_PER_IMAGE = 100
- 
-    # Use a small epoch since the data is simple
     STEPS_PER_EPOCH = 50
- 
-    # use small validation steps since the epoch is small
     VALIDATION_STEPS = 50
- 
- 
-config = ShapesConfig()
+
+config = FactionsConfig()
 config.display()
- 
- 
+
 class DrugDataset(utils.Dataset):
-    # 得到该图中有多少个实例（物体）
     def get_obj_index(self, image):
         n = np.max(image)
         return n
- 
-    # 解析labelme中得到的yaml文件，从而得到mask每一层对应的实例标签
+
     def from_yaml_get_class(self, image_id):
         info = self.image_info[image_id]
         with open(info['yaml_path']) as f:
@@ -92,48 +51,25 @@ class DrugDataset(utils.Dataset):
             labels = temp['label_names']
             del labels[0]
         return labels
- 
-    # 重新写draw_mask
+
     def draw_mask(self, num_obj, mask, image, image_id):
-        # print("draw_mask-->",image_id)
-        # print("self.image_info",self.image_info)
         info = self.image_info[image_id]
-        # print("info-->",info)
-        # print("info[width]----->",info['width'],"-info[height]--->",info['height'])
         for index in range(num_obj):
             for i in range(info['width']):
                 for j in range(info['height']):
-                    # print("image_id-->",image_id,"-i--->",i,"-j--->",j)
-                    # print("info[width]----->",info['width'],"-info[height]--->",info['height'])
                     at_pixel = image.getpixel((i, j))
                     if at_pixel == index + 1:
                         mask[j, i, index] = 1
         return mask
- 
-    # 重新写load_shapes，里面包含自己的自己的类别
-    # 并在self.image_info信息中添加了path、mask_path 、yaml_path
-    # yaml_pathdataset_root_path = "/dateset/"
-    # img_floder = dataset_root_path + "rgb"
-    # mask_floder = dataset_root_path + "mask"
-    # dataset_root_path = "/tongue_dateset/"
-    def load_shapes(self, count, img_floder, mask_floder, imglist, dataset_root_path):
-        """Generate the requested number of synthetic images.
-        count: number of images to generate.
-        height, width: the size of the generated images.
-        """
 
-        # Customize
+    def load_shapes(self, count, img_floder, mask_floder, imglist, dataset_root_path):
         # Add classes
         self.add_class("shapes", 1, "BLUFOR")
         self.add_class("shapes", 2, "REDFOR")
  
         for i in range(count):
-            # 获取图片宽和高
             print(i)
             filestr = imglist[i].split(".")[0]
-            # print(imglist[i],"-->",cv_img.shape[1],"--->",cv_img.shape[0])
-            # print("id-->", i, " imglist[", i, "]-->", imglist[i],"filestr-->",filestr)
-            # filestr = filestr.split("_")[1]
             mask_path = mask_floder + "/" + filestr + ".png"
             yaml_path = dataset_root_path + "labelme_json/" + filestr + "_json/info.yaml"
             print(dataset_root_path + "labelme_json/" + filestr + "_json/img.png")
@@ -142,11 +78,8 @@ class DrugDataset(utils.Dataset):
  
             self.add_image("shapes", image_id=i, path=img_floder + "/" + imglist[i],
                            width=cv_img.shape[1], height=cv_img.shape[0], mask_path=mask_path, yaml_path=yaml_path)
- 
-    # 重写load_mask
+
     def load_mask(self, image_id):
-        """Generate instance masks for shapes of the given image ID.
-        """
         global iter_num
         print("image_id", image_id)
         info = self.image_info[image_id]
@@ -164,7 +97,6 @@ class DrugDataset(utils.Dataset):
         labels = self.from_yaml_get_class(image_id)
         labels_form = []
 
-        # Customize
         for i in range(len(labels)):
             if labels[i].find("BLUFOR") != -1:
                 labels_form.append("BLUFOR")
@@ -174,47 +106,21 @@ class DrugDataset(utils.Dataset):
 
         class_ids = np.array([self.class_names.index(s) for s in labels_form])
         return mask, class_ids.astype(np.int32)
- 
-'''
-def get_ax(rows=1, cols=1, size=8):
-    """Return a Matplotlib Axes array to be used in
-    all visualizations in the notebook. Provide a
-    central point to control graph sizes.
-    Change the default size attribute to control the size
-    of rendered images
-    """
-    _, ax = plt.subplots(rows, cols, figsize=(size * cols, size * rows))
-    return ax
-'''
 
-# Customize
-# 基础设置
-dataset_root_path = "data/dataset/faction_2/"    # 你的数据的路径
+
+dataset_root_path = "data/dataset/faction_2/"   # dataset root path
 img_floder = dataset_root_path + "pic"
 mask_floder = dataset_root_path + "cv2_mask"
-# yaml_floder = dataset_root_path
 imglist = os.listdir(img_floder)
 count = len(imglist)
- 
-# train与val数据集准备
+
 dataset_train = DrugDataset()
 dataset_train.load_shapes(count, img_floder, mask_floder, imglist, dataset_root_path)
 dataset_train.prepare()
  
-# print("dataset_train-->",dataset_train._image_ids)
- 
 dataset_val = DrugDataset()
 dataset_val.load_shapes(count, img_floder, mask_floder, imglist, dataset_root_path)
 dataset_val.prepare()
- 
-# print("dataset_val-->",dataset_val._image_ids)
- 
-# Load and display random samples
-# image_ids = np.random.choice(dataset_train.image_ids, 4)
-# for image_id in image_ids:
-#    image = dataset_train.load_image(image_id)
-#    mask, class_ids = dataset_train.load_mask(image_id)
-#    visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
  
 # Create model in training mode
 model = modellib.MaskRCNN(mode="training", config=config,
@@ -229,8 +135,8 @@ elif init_with == "coco":
     # Load weights trained on MS COCO, but skip layers that
     # are different due to the different number of classes
     # See README for instructions to download the COCO weights
-    # print(COCO_MODEL_PATH)
-    model.load_weights(COCO_MODEL_PATH, by_name=True,
+    # print(PEOPLE_MODEL_PATH)
+    model.load_weights(PEOPLE_MODEL_PATH, by_name=True,
                        exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
                                 "mrcnn_bbox", "mrcnn_mask"])
 elif init_with == "last":
@@ -245,7 +151,7 @@ model.train(dataset_train, dataset_val,
             learning_rate=config.LEARNING_RATE,
             epochs=10,
             layers='heads')
- 
+
 # Fine tune all layers
 # Passing layers="all" trains all layers. You can also
 # pass a regular expression to select which layers to
